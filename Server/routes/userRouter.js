f9392c177c2695aab0d2c27932fa89ac51eef6eb
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 16:16:03 +0530

    Add getCurrentUser route in userRouter

--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -1,10 +1,16 @@
 // Server / routes / userRouter.js
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  getCurrentUser,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+userRouter.get("/profile", authMiddleware, getCurrentUser);
 
 export default userRouter;
