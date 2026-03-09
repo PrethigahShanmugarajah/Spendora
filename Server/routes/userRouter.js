693df555a4eb1a2fc595d822ca9730ba1b42feb7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 16:29:36 +0530

    Add updateProfile route in userRouter

--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -4,6 +4,7 @@ import {
   getCurrentUser,
   loginUser,
   registerUser,
+  updateProfile,
 } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/auth.js";
 
@@ -12,5 +13,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/profile", authMiddleware, getCurrentUser);
+userRouter.put("/profile", authMiddleware, updateProfile);
 
 export default userRouter;
