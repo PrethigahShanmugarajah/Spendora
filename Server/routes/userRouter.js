fd15a6f2abfb30c5a2be5defca604a4353a22f38
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 15:49:54 +0530

    Add registerUser route in userRouter

--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -1,1 +1,9 @@
 // Server / routes / userRouter.js
+import express from "express";
+import { registerUser } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+
+export default userRouter;
