d72c20f233dded3ffc30bd3ef73f0c2fa24eb757
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 17:49:53 +0530

    Add getIncomeById route in incomeRouter

--- Server/routes/incomeRouter.js
+++ Server/routes/incomeRouter.js
@@ -1,11 +1,16 @@
 // Server / routes / incomeRouter.js
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.js";
-import { addIncome, getIncomes } from "../controllers/incomeController.js";
+import {
+  addIncome,
+  getIncomeById,
+  getIncomes,
+} from "../controllers/incomeController.js";
 
 const incomeRouter = express.Router();
 
 incomeRouter.post("/add", authMiddleware, addIncome);
 incomeRouter.get("/get", authMiddleware, getIncomes);
+incomeRouter.get("/get/:id", authMiddleware, getIncomeById);
 
 export default incomeRouter;
