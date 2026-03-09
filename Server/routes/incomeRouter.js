e1032afefd9adcddd88b77ef4af0adfd4929f89a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 17:38:59 +0530

    Add addIncome route in incomeRouter

--- Server/routes/incomeRouter.js
+++ Server/routes/incomeRouter.js
@@ -1,1 +1,10 @@
 // Server / routes / incomeRouter.js
+import express from "express";
+import { authMiddleware } from "../middlewares/auth.js";
+import { addIncome } from "../controllers/incomeController.js";
+
+const incomeRouter = express.Router();
+
+incomeRouter.post("/add", authMiddleware, addIncome);
+
+export default incomeRouter;
