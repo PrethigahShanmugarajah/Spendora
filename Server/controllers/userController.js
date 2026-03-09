4f9021f861dc2e517527ec27a97a8c7f7cedc96c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 16:01:26 +0530

    Add function for User Login in userController

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -127,3 +127,68 @@ export async function registerUser(req, res) {
     });
   }
 }
+
+/* -------- User Login -------- */
+export async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // if (!email || !password) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "Email and password are required.",
+    //   });
+    // }
+
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is required.",
+      });
+    }
+
+    if (!password) {
+      return res.status(400).json({
+        success: false,
+        message: "Password is required.",
+      });
+    }
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "No account found with this email.",
+      });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect password. Please try again.",
+      });
+    }
+
+    const token = createToken(user._id);
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully.",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error("User Login Error:", error?.stack || error?.message || error);
+
+    return res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while logging in.",
+      error: `User Login Error: ${error?.stack || error?.message || error}`,
+    });
+  }
+}
