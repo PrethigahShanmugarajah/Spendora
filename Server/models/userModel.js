449fe2168cfae636506d6ac521f469b8351929d1
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 14:12:36 +0530

    Add user model schema

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,1 +1,21 @@
 // Server / models / userModel.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+export default userModel;
