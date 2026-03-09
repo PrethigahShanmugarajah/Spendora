9be7cdb070584410d48a4db9ed7563f2848cc83d
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 17:49:37 +0530

    Add function for Get Income in incomeController

--- Server/controllers/incomeController.js
+++ Server/controllers/incomeController.js
@@ -95,3 +95,40 @@ export async function getIncomes(req, res) {
     });
   }
 }
+
+/* -------- Get Income -------- */
+export async function getIncomeById(req, res) {
+  try {
+    const userId = req.user._id;
+    const incomeId = req.params.id;
+
+    if (!incomeId) {
+      return res.status(400).json({
+        success: false,
+        message: "Income ID is required.",
+      });
+    }
+
+    const income = await incomeModel.findOne({ _id: incomeId, userId });
+    if (!income) {
+      return res.status(404).json({
+        success: false,
+        message: "Income not found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Income fetched successfully.",
+      income,
+    });
+  } catch (error) {
+    console.error("Get Income Error:", error?.stack || error?.message || error);
+
+    return res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while fetching the income.",
+      error: `Get Income Error: ${error?.stack || error?.message || error}`,
+    });
+  }
+}
