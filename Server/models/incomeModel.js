03f5f23872ad5506dfad2b4268881edc35606d31
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-09 17:37:16 +0530

    Add income model schema

--- Server/models/incomeModel.js
+++ Server/models/incomeModel.js
@@ -1,1 +1,34 @@
 // Server / models / incomeModel.js
+import mongoose from "mongoose";
+
+const incomeSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "income",
+  },
+});
+
+const incomeModel =
+  mongoose.models.income || mongoose.model("income", incomeSchema);
+export default incomeModel;
