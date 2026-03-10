4845164187867d8dc9fa8f75c742f6b9047cae98
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 06:56:54 +0530

    Add timestamps to income model schema

--- Server/models/incomeModel.js
+++ Server/models/incomeModel.js
@@ -1,33 +1,36 @@
 // Server / models / incomeModel.js
 import mongoose from "mongoose";
 
-const incomeSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
+const incomeSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "income",
+    },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  type: {
-    type: String,
-    default: "income",
-  },
-});
+  { timestamps: true },
+);
 
 const incomeModel =
   mongoose.models.income || mongoose.model("income", incomeSchema);
