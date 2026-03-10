9a03179d81145794fa66e82426b4aa3bb8c2ce5a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 07:06:08 +0530

    Add expenseRouter

--- Server/Server.js
+++ Server/Server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRouter.js";
 import incomeRouter from "./routes/incomeRouter.js";
+import expenseRouter from "./routes/expenseRouter.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("API is Working!"));
 app.use("/api/user", userRouter);
 app.use("/api/income", incomeRouter);
+app.use("/api/expense", expenseRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 4000;
