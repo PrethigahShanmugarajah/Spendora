9a9e696edc645e710977ba1e5808262bbe751099
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 07:20:21 +0530

    Add getExpenseById route in expenseRouter

--- Server/routes/expenseRouter.js
+++ Server/routes/expenseRouter.js
@@ -1,11 +1,16 @@
 // Server / routes / expenseRouter.js
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.js";
-import { addExpense, getExpenses } from "../controllers/expenseController.js";
+import {
+  addExpense,
+  getExpenseById,
+  getExpenses,
+} from "../controllers/expenseController.js";
 
 const expenseRouter = express.Router();
 
 expenseRouter.post("/add", authMiddleware, addExpense);
 expenseRouter.get("/get", authMiddleware, getExpenses);
+expenseRouter.get("/get/:id", authMiddleware, getExpenseById);
 
 export default expenseRouter;
