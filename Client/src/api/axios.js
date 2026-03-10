b0ccee0f2a11e7049aeedd361ab7290f02781cab
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 14:52:04 +0530

    Add axios instance with baseURL and token interceptor

---
+++ Client/src/api/axios.js
@@ -0,0 +1,23 @@
+// Client / src / api / axios.js
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASEURL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default api;
