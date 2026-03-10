a041b1b787ea1354cb63b187b09e687eed817f95
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 07:05:24 +0530

    Add function for Add Expense in expenseController

--- Server/controllers/expenseController.js
+++ Server/controllers/expenseController.js
@@ -1,1 +1,73 @@
 // Server / controllers / expenseController.js
+import expenseModel from "../models/expenseModel.js";
+
+/* -------- Add Expense -------- */
+export async function addExpense(req, res) {
+  try {
+    const userId = req.user._id;
+    const { description, amount, category, date } = req.body;
+
+    // if (!description || !amount || !category || !date) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     message:
+    //       "All fields (description, amount, category, date) are required.",
+    //   });
+    // }
+
+    if (!description) {
+      return res.status(400).json({
+        success: false,
+        message: "Description is required.",
+      });
+    }
+
+    if (!amount) {
+      return res.status(400).json({
+        success: false,
+        message: "Amount is required.",
+      });
+    }
+
+    if (!category) {
+      return res.status(400).json({
+        success: false,
+        message: "Category is required.",
+      });
+    }
+
+    if (!date) {
+      return res.status(400).json({
+        success: false,
+        message: "Date is required.",
+      });
+    }
+
+    const newExpense = new expenseModel({
+      userId,
+      description,
+      amount,
+      category,
+      date: new Date(date),
+    });
+
+    await newExpense.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Expense added successfully.",
+      expense: newExpense,
+    });
+  } catch (error) {
+    console.error(
+      "Add Expense Error:",
+      error?.stack || error?.message || error,
+    );
+
+    return res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred while adding expense.",
+      error: `Add Expense Error: ${error?.stack || error?.message || error}`,
+    });
+  }
+}
