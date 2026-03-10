1b3ab171064c058ec1ccb9b0a4b6a70b9020dccb
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 07:05:44 +0530

    Add addExpense route in expenseRouter

--- Server/routes/expenseRouter.js
+++ Server/routes/expenseRouter.js
@@ -1,1 +1,10 @@
 // Server / routes / expenseRouter.js
+import express from "express";
+import { authMiddleware } from "../middlewares/auth.js";
+import { addExpense } from "../controllers/expenseController.js";
+
+const expenseRouter = express.Router();
+
+expenseRouter.post("/add", authMiddleware, addExpense);
+
+export default expenseRouter;
