98614e1b8b68ae05855cbbbb1a85dcb59a40519f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 07:26:22 +0530

    Add updateExpense route in expenseRouter

--- Server/routes/expenseRouter.js
+++ Server/routes/expenseRouter.js
@@ -5,6 +5,7 @@ import {
   addExpense,
   getExpenseById,
   getExpenses,
+  updateExpense,
 } from "../controllers/expenseController.js";
 
 const expenseRouter = express.Router();
@@ -12,5 +13,6 @@ const expenseRouter = express.Router();
 expenseRouter.post("/add", authMiddleware, addExpense);
 expenseRouter.get("/get", authMiddleware, getExpenses);
 expenseRouter.get("/get/:id", authMiddleware, getExpenseById);
+expenseRouter.put("/update/:id", authMiddleware, updateExpense);
 
 export default expenseRouter;
