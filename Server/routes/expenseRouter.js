ae4a177b39a43734b0632c6afed893739c6fb964
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-10 07:14:20 +0530

    Add getExpenses route in expenseRouter

--- Server/routes/expenseRouter.js
+++ Server/routes/expenseRouter.js
@@ -1,10 +1,11 @@
 // Server / routes / expenseRouter.js
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.js";
-import { addExpense } from "../controllers/expenseController.js";
+import { addExpense, getExpenses } from "../controllers/expenseController.js";
 
 const expenseRouter = express.Router();
 
 expenseRouter.post("/add", authMiddleware, addExpense);
+expenseRouter.get("/get", authMiddleware, getExpenses);
 
 export default expenseRouter;
